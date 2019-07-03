99a3ec3f6fc09a3ccb746cc1358a749cd180d9aa
Shion-0014 <seanmqe@gmail.com>
2019-07-03 13:09:59 +0900

    ADD: blackboard detector

---
+++ settings.py
@@ -0,0 +1,9 @@
+Args = {'NUM_BLACKBOARD': 1,
+        'LOW_RANGE': [20, 20, 20],
+        'HIGH_RANGE': [140, 140, 140],
+        'THRES': 50,
+        'INPUT_PATH': 'input/demo_sample1.mp4',
+        'OUTPUT_PATH': 'output/demo_brack_board1s.mp4',
+        'X_SIZE_RATIO': 1,
+        'Y_SIZE_RATIO': 1
+        }
